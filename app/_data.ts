5d42ab25b6ce3782d0347e2eb6e11566ed74bae7
James L. Johnson III <jamesljohnson3@gmail.com>
2024-01-16 12:32:25 -0500

    Update _data.ts

--- app/_data.ts
+++ app/_data.ts
@@ -1,7 +1,7 @@
 export const data = [
   {
     id: "1",
-    name: "bleach",
+    name: "booty",
     image: {
       original:
         "https://m.media-amazon.com/images/M/MV5BZjE0YjVjODQtZGY2NS00MDcyLThhMDAtZGQwMTZiOWNmNjRiXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg",
